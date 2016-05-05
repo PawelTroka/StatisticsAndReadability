4a607b66ab666a20a0d99453462ae2af51a27726
PTroka <pawel.troka@outlook.com>
2016-05-05 02:33:42 +0200

    Added simple WordDocument showing StatisticsAndReadability info on start.

--- StatisticsAndReadabilityWordDocument/Properties/AssemblyInfo.cs
+++ StatisticsAndReadabilityWordDocument/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("StatisticsAndReadability")]
-[assembly: AssemblyDescription("Simple but useful Add-In for Microsoft Word. Provides statistical and readability informations about opened document on it's own ribbon tab with option to recalculate after changes.")]
+[assembly: AssemblyTitle("StatisticsAndReadabilityWordDocument")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pawel Troka")]
-[assembly: AssemblyProduct("StatisticsAndReadability")]
+[assembly: AssemblyProduct("StatisticsAndReadabilityWordDocument")]
 [assembly: AssemblyCopyright("Copyright ©  2016 Pawel Troka")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,12 +18,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("01e7641e-5d9e-4b81-952f-47af2174b161")]
+[assembly: Guid("c77cea67-20ab-4e67-8ec9-d409eb3dc717")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ StatisticsAndReadabilityWordDocument/StatisticsAndReadabilityDocument.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Office.Tools.Word;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace StatisticsAndReadabilityWordDocument
+{
+    public partial class StatisticsAndReadabilityDocument
+    {
+        private void StatisticsAndReadabilityDocument_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.DocumentOpen += Application_DocumentOpen;
+        }
+
+        private void Application_DocumentOpen(Word.Document Doc)
+        {
+         //   Object start = Doc.Content.Start;
+         //   Object end = Doc.Content.End;
+          //  Doc.Range(ref start, ref end).Select();
+            if (string.IsNullOrEmpty(Doc.Content.Text)||string.IsNullOrWhiteSpace(Doc.Content.Text))
+            {
+                var s = File.ReadAllText(System.IO.Path.Combine(Application.CustomDictionaries.ActiveCustomDictionary.Path, Application.CustomDictionaries.ActiveCustomDictionary.Name) );
+                Doc.Content.Text =s;
+            }
+            try
+            {
+                var sb = new StringBuilder("");
+                foreach (Word.ReadabilityStatistic readabilityStatistic in Doc.ReadabilityStatistics)
+                {
+                    sb.AppendLine($"{readabilityStatistic.Name}: {readabilityStatistic.Value}");
+                }
+                MessageBox.Show(sb.ToString(), "STATISTICS AND READABILITY");
+            }
+            catch { }
+        }
+
+        private void StatisticsAndReadabilityDocument_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(StatisticsAndReadabilityDocument_Startup);
+            this.Shutdown += new System.EventHandler(StatisticsAndReadabilityDocument_Shutdown);
+        }
+
+        #endregion
+    }
+}
